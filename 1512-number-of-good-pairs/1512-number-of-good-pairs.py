b15fdd60d20ae907ffce4f864baa3e67830c3749
Ayesha Andleeb <136998971+AyeshaAndleeb@users.noreply.github.com>
2025-06-29 22:43:28 +0500

    Time: 0 ms (100%), Space: 18 MB (9.33%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,8 +1,9 @@
+from collections import defaultdict
 class Solution:
-    def numIdenticalPairs(self, nums: List[int]) -> int:  
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        freq = defaultdict(int)
         count = 0
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] == nums[j]:
-                    count+=1
+        for num in nums:
+            count += freq[num]   # add how many times we've seen this number before
+            freq[num] += 1       # update frequency
         return count
