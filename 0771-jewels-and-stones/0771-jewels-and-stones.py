d292ae50a39541654762404bd6b1b82e5b9c5b83
Ayesha Andleeb <136998971+AyeshaAndleeb@users.noreply.github.com>
2026-01-13 13:11:37 +0500

    Time: 47 ms (0.12%), Space: 19.4 MB (6.3%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,10 +1,3 @@
 class Solution:
     def numJewelsInStones(self, J: str, S: str) -> int:
-        jewel_set = set(J)  # fast lookup
-        count = 0
-        
-        for stone in S:
-            if stone in jewel_set:
-                count += 1
-        
-        return count
+        return sum(S.count(jewel) for jewel in J)
