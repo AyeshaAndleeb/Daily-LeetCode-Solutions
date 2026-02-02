39240400c4508c83604cbe75963134c87ffcf47b
Ayesha Andleeb <136998971+AyeshaAndleeb@users.noreply.github.com>
2026-02-02 15:26:32 +0500

    Time: 0 ms (100%), Space: 19.5 MB (15.54%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.py
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.py
@@ -1,9 +1,17 @@
 class Solution:
     def minimumCost(self, nums):
-        total_cost = nums[0]        # pehla element hamesha add
-        rest = nums[1:]             # baki array
-        rest.sort()                 # chote se bada sort kar do
-        total_cost += rest[0]       # second subarray ka smallest
-        total_cost += rest[1]       # third subarray ka next smallest
-        return total_cost
+        total_cost = nums[0]
 
+        first_min = float('inf')
+        second_min = float('inf')
+
+        for num in nums[1:]:
+            if num < first_min:
+                second_min = first_min   
+                first_min = num
+            elif num < second_min:
+                second_min = num
+
+        total_cost += first_min + second_min
+
+        return total_cost
